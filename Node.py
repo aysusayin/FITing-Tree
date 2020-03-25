a942d4643355c8b38f1eb90184423d5984eac48c
Aysu Sayin <aysusayin@Munny.local>
2020-03-25 15:44:49 +0300

    Initialization of the data structure

---
+++ Node.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self):
+        self.keys = []
+        self.values = []
+        self.leaf = True
+        self.child_num = 0
+
+    def add_children(self, node):
+        pass
+
+    def set_data(self, data):
+        pass
