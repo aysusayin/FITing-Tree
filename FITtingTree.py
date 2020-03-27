b4bebf61282b3a45d0abde2528f4653ad42729e6
Aysu Sayin <aysusayin@Munny.local>
2020-03-27 20:46:37 +0300

    implemented shrinking code algorithm

--- FITtingTree.py
+++ FITtingTree.py
@@ -1,1 +1,49 @@
 ''' A generic FITting tree'''
+
+import bisect
+
+from Node import Leaf, Node, Segment
+
+
+class FITtingTree:
+    def __init__(self, error, buffer_error, branching_factor=16):
+        self.branching_factor = branching_factor
+        self.leaves = Leaf(None, None, None, branching_factor) # Linked List of leaves
+        self.root = self.leaves
+        self.error = error - buffer_error
+        self.buffer_error = buffer_error
+
+    def shrinking_cone(self, keys, locs):
+        # data is the keys and pointers , it must be sorted
+        segments = []
+        high_slope = float('inf')
+        low_slope = 0
+        origin_key = keys[0]
+        origin_loc = locs[0]
+        end_key = keys[0]
+        end_loc = locs[0]
+        for (key,loc) in list(zip(keys[1:], locs[1:])):
+            tmp_point_slope = (loc - origin_loc) / (key - origin_key)
+            if low_slope < tmp_point_slope < high_slope:
+                # Point is inside the cone
+                tmp_high_slope = ((loc + self.error) - origin_loc) / (key - origin_key)
+                tmp_low_slope = ((loc - self.error) - origin_loc) / (key - origin_key)
+                high_slope = min(high_slope, tmp_high_slope)
+                low_slope = max(low_slope, tmp_low_slope)
+                end_key = key
+                end_loc = loc
+            else:
+                new_segment = Segment(high_slope, low_slope,
+                                      (origin_key, origin_loc),
+                                      (end_key, end_loc))
+                high_slope = float('inf')
+                low_slope = 0
+                origin_key = key
+                origin_loc = loc
+                end_key = key
+                origin_loc = loc
+                segments.append(new_segment)
+
+        return segments
+
+
--- Node.py
+++ Node.py
@@ -1,12 +1,32 @@
-class Node:
-    def __init__(self):
+import collections
+
+
+class Leaf:
+    def __init__(self, previous_leaf, next_leaf, parent, branching_factor=16):
+        self.previous = previous_leaf
+        self.next = next_leaf
+        self.parent = parent
+        self.branching_factor = branching_factor
         self.keys = []
-        self.values = []
-        self.leaf = True
-        self.child_num = 0
+        self.children = []  # (key, value)
 
-    def add_children(self, node):
-        pass
 
-    def set_data(self, data):
-        pass
+class Node:
+    def __init__(self, previous_node, next_node, keys, children, parent=None, branching_factor=16):
+        self.previous = previous_node
+        self.next = next_node
+        self.keys = keys  # NOTE: must keep keys sorted
+        self.children = children  # NOTE: children must correspond to parents.
+        self.parent = parent
+        self.branching_factor = branching_factor
+        for child in children:
+            child.parent = self
+
+
+class Segment:
+    def __init__(self, high_slope, low_slope, start, end):
+        self.high_slope = high_slope
+        self.low_slope = low_slope
+        self.start_point = start  # (key, location) tuple
+        self.end_point = end
+        self.buffer = collections.deque()
