691f059057ecc718ec3d8ef59982856a33ba1ead
aysusayin <aysusayin@gmail.com>
2020-06-23 00:34:49 +0300

    Added a description for input file format

--- constants.py
+++ constants.py
@@ -1,21 +1,32 @@
-DATABASE_LOCATION = 'covid/data_files/fitting/'
-DATABASE_NAME = DATABASE_LOCATION + 'my_fitting_db'
+DATABASE_LOCATION = '/'
+DATABASE_NAME = DATABASE_LOCATION + ''
 BUFFER_ERROR = 20
 ERROR = 25
 KEY_SIZE = 8
-FIELD_SIZE = 10 # fields are double so size needs to be 8-10 bytes
-FIELD_NUM = 7  #COVID: ['day', 'month', 'year', 'cases', 'deaths', 'countryterritoryCode', 'popData2018']
+FIELD_SIZE = 10
+FIELD_NUM = 10
 RECORD_SIZE = KEY_SIZE + FIELD_NUM * FIELD_SIZE
 BUFFER_SIZE = BUFFER_ERROR * RECORD_SIZE
 ALL_FIELDS_SIZE = RECORD_SIZE - KEY_SIZE  # size of the all fields except for key
-FIELD_TYPES = ['float']
-FIELD_TYPE = 'float'
+FIELD_TYPES = ['string']
+FIELD_TYPE = 'string'
 
 
 def read_from_file(file_path):
+    """
+    Metadata file format:
+    Database location
+    Database Name
+    Key Size
+    Field Size
+    Number of Fields
+    Types of Fields : A list of types. Unfortunately only one type for all fields is supported now.
+    Buffer Error
+    Error: Total error which includes buffer error in it
+    """
     input_file = open(file_path, 'r+')
     global DATABASE_LOCATION, DATABASE_NAME, KEY_SIZE, FIELD_SIZE, FIELD_NUM, \
-        RECORD_SIZE, ALL_FIELDS_SIZE, BUFFER_ERROR, ERROR, BUFFER_SIZE, FIELD_TYPES
+        RECORD_SIZE, ALL_FIELDS_SIZE, BUFFER_ERROR, ERROR, BUFFER_SIZE, FIELD_TYPES, FIELD_TYPE
     DATABASE_LOCATION = str(input_file.readline()).rstrip() + 'fitting/'
     DATABASE_NAME = DATABASE_LOCATION + str(input_file.readline()).rstrip()
     KEY_SIZE = int(input_file.readline())
